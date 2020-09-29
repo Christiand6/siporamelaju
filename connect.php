23c6c6e27a0a926d61e1149626243d4f339ddf28
Christiand6 <34437533+Christiand6@users.noreply.github.com>
2020-09-29 20:57:22 +0700

    Update connect.php

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+	$name = $_POST['name'];
+	$age= $_POST['age'];
+	$job = $_POST['job'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$hope = $_POST['hope'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','sijuhre_papa');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(name, age, job, email, phone, hope) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sissis", $name, $age, $job, $email, $phone, $hope);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
